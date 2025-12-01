202fd615d29b5a8222be514255cf9764728b2de6
D <dector@dector.space>
2025-12-01 21:57:02 +0100

    feat: parse config and list all tasks

--- cmd/ror/main.go
+++ cmd/ror/main.go
@@ -1,7 +1,99 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/dector/kdly"
+)
+
+var configFile = "ror.kdl"
+
+type Task struct {
+	Name        string
+	Command     string
+	Description string
+}
+
+type RunnerConfig struct {
+	Tasks map[string]Task
+}
 
 func main() {
-	fmt.Printf("OK\n")
+	config := buildConfig()
+
+	// Validation
+	if len(config.Tasks) == 0 {
+		fmt.Println("No tasks found in ror.kdl")
+		return
+	}
+
+	// Print tasks
+	fmt.Println("Available tasks:")
+	for name, task := range config.Tasks {
+		desc := task.Description
+		fmt.Printf("  %s: %s\n", name, desc)
+	}
+}
+
+func buildConfig() RunnerConfig {
+	doc, err := parseConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	config := RunnerConfig{
+		Tasks: make(map[string]Task),
+	}
+
+	// Iterate over top-level nodes
+	for _, node := range doc.Nodes {
+		if node.Name != "task" {
+			panic(fmt.Sprintf("unknown top-level node: %s", node.Name))
+		}
+
+		if len(node.Arguments) < 1 {
+			panic("task node requires a name argument")
+		}
+
+		taskName := node.Arguments[0].Value
+
+		task := Task{
+			Name: taskName,
+		}
+
+		if node.Children != nil {
+			for _, child := range node.Children {
+				switch child.Name {
+				case "cmd":
+					if len(child.Arguments) > 0 {
+						task.Command = child.Arguments[0].Value
+					}
+				case "description":
+					if len(child.Arguments) > 0 {
+						task.Description = child.Arguments[0].Value
+					}
+				default:
+					panic(fmt.Sprintf("unknown property in task '%s': %s", taskName, child.Name))
+				}
+			}
+		}
+
+		config.Tasks[taskName] = task
+	}
+
+	return config
+}
+
+func parseConfig() (*kdly.Document, error) {
+	content, err := os.ReadFile(configFile)
+	if err != nil {
+		if os.IsNotExist(err) {
+			fmt.Println("ror.kdl not found")
+			os.Exit(1)
+		}
+		return nil, err
+	}
+
+	return kdly.Parse(string(content))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/dector/ror
 
 go 1.25.4
+
+require github.com/dector/kdly v0.0.0-20251130231056-5b5a81e522eb
