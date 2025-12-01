8d56d4337c163aedd3ebe80a8addc91263db5b42
D <dector@dector.space>
2025-12-01 22:27:23 +0100

    feat: execute command dependencies

--- cmd/ror/main.go
+++ cmd/ror/main.go
@@ -30,39 +30,50 @@ func main() {
 	}
 
 	taskName := os.Args[1]
-	if err := runTask(taskName, config); err != nil {
+	executed := make(map[string]bool)
+	visiting := make(map[string]bool)
+	if err := runTask(taskName, config, executed, visiting); err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
 		os.Exit(1)
 	}
 }
 
-func runTask(name string, config RunnerConfig) error {
+func runTask(name string, config RunnerConfig, executed map[string]bool, visiting map[string]bool) error {
+	if visiting[name] {
+		return fmt.Errorf("circular dependency detected: %s", name)
+	}
+	if executed[name] {
+		return nil
+	}
+
+	visiting[name] = true
+	defer func() { delete(visiting, name) }()
+
 	task, ok := config.Tasks[name]
 	if !ok {
 		return fmt.Errorf("task '%s' not found", name)
 	}
 
 	for _, dep := range task.DependsOn {
-		if err := runTask(dep, config); err != nil {
+		if err := runTask(dep, config, executed, visiting); err != nil {
 			return err
 		}
 	}
 
-	if task.Command == "" {
-		return nil
+	if task.Command != "" {
+		fmt.Printf("Running task: %s\n", name)
+		// TODO: Support other shells or direct execution
+		cmd := exec.Command("sh", "-c", task.Command)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		cmd.Stdin = os.Stdin
+
+		if err := cmd.Run(); err != nil {
+			return fmt.Errorf("failed to run task '%s': %w", name, err)
+		}
 	}
 
-	fmt.Printf("Running task: %s\n", name)
-	// TODO: Support other shells or direct execution
-	cmd := exec.Command("sh", "-c", task.Command)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	cmd.Stdin = os.Stdin
-
-	if err := cmd.Run(); err != nil {
-		return fmt.Errorf("failed to run task '%s': %w", name, err)
-	}
-
+	executed[name] = true
 	return nil
 }
 
